4a0f3e8e9447d293f0c4690600e85baa24e553d1
muhaimin97 <muhaimin271997@gmail.com>
2022-11-17 12:21:14 +0800

    update AppTest.java ip address

--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest
 {
 	WebDriver driver; 
 	WebDriverWait wait; 
-	String url = "http://172.17.0.2";
+	String url = "http://192.168.31.53";
 	String validEmail = "user@example.com";
 	String validPassword = "password1234";
 	String invalidEmail = "none@example.com";
